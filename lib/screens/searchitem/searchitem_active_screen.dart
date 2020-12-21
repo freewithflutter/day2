c47b04c43683974dce61e380b06d28324a6be664
Hj L\ee <hj_lee_com@naver.com>
2020-12-21 22:19:28 +0900

    ha,,,

---
+++ lib/screens/searchitem/searchitem_active_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchItemActive extends StatefulWidget {
+  @override
+  _SearchItemActiveState createState() => _SearchItemActiveState();
+}
+
+class _SearchItemActiveState extends State<SearchItemActive> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('wowo'),
+    );
+  }
+}
