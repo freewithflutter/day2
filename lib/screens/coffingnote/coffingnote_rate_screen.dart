14f154375a4c4b213b7ca7288b11e0bfd8c0893a
Hj L\ee <hj_lee_com@naver.com>
2020-12-30 14:14:01 +0900

    only last the last page

--- lib/screens/coffingnote/coffingnote_rate_screen.dart
+++ lib/screens/coffingnote/coffingnote_rate_screen.dart
@@ -15,13 +15,17 @@ class _CoffingNoteRateState extends State<CoffingNoteRate> {
         iconTheme: IconThemeData(
           color: kTextDarkGaryColor,
         ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         actions: [
           FlatButton(
+            onPressed: () {},
             child: Container(
               child: Text(
                 'Reset',
                 style: TextStyle(
                   fontSize: 20,
+                  fontWeight: FontWeight.bold,
                   color: Color(0xFFEBBB9D),
                 ),
               ),
@@ -29,11 +33,260 @@ class _CoffingNoteRateState extends State<CoffingNoteRate> {
           ),
         ],
       ),
+      extendBodyBehindAppBar: true,
       body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 20),
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/coffingmainbg.png'),
             fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Container(
+            child: Column(
+              children: [
+                //TODO sectino 1 starts
+                Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        'RATING',
+                        style: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w900,
+                          color: Color(0xFF555454),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 21,
+                      ),
+                      Container(
+                        child: Image.asset(
+                          'assets/images/roasteryitem1.png',
+                          width: 125,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(
+                          top: 26,
+                          bottom: 21,
+                        ),
+                        child: Text(
+                          '프리츠 커피 컴퍼니',
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.w800,
+                            color: Color(0xFF555454),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                //TODO section1 done => section2 strats
+                Container(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text(
+                          'Preference',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Color(0xFF625C5C),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 58,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Color(0xFFEBBB9D),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'GOOD',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 26,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 58,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
+                                border: Border.all(
+                                  width: 1,
+                                  color: Color(0xFFDFDFDF),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'BAD',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 26,
+                                    color: Color(0xFF555454),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text(
+                          'Cup note',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Color(0xFF625C5C),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Color(0xFFF9D9C5),
+                                border: Border.all(
+                                  width: 2,
+                                  color: Color(0xFFEBBB9D),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Nutty',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color(0xFF555454),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.only(right: 10),
+                              height: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
+                                border: Border.all(
+                                  width: 1,
+                                  color: Color(0xFFDFDFDF),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Sweety',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    color: Color(0xFF555454),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.only(right: 10),
+                              height: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
+                                border: Border.all(
+                                  width: 1,
+                                  color: Color(0xFFDFDFDF),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Sweety',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    color: Color(0xFF555454),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 45,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
+                                border: Border.all(
+                                  width: 1,
+                                  color: Color(0xFFDFDFDF),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Sweety',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    color: Color(0xFF555454),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text(
+                          'Minimal Rating 좋았던 맛의 강도',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Color(0xFF625C5C),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
